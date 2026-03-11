165e98c42ba0e2b9d76c75259fabe0367626c724
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-12 00:49:23 +0600

    aleart done

--- src/Components/WishListBooks/WishListBooks.jsx
+++ src/Components/WishListBooks/WishListBooks.jsx
@@ -2,49 +2,81 @@ import React, { useEffect, useState } from 'react';
 import { getStoredBook, removeDetails } from '../../Utlity/Utlity';
 import { useLoaderData } from 'react-router';
 import WishListBook from '../WishListBook/WishListBook';
+import Swal from 'sweetalert2';
 
 const WishListBooks = () => {
-    const [book, setBook] = useState([])
+
+    const [book, setBook] = useState([]);
     const allBooks = useLoaderData();
-    // console.log(allBooks);
 
 
-
-    // handle remove;
+    // remove book
     const handleRemove = (id) => {
-        // console.log(id);
-        const reminingBook = book.filter(b=>b.bookId !== id)
-        setBook(reminingBook)
-        removeDetails(id)
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "This book will be removed!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#22c55e",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, remove it!"
+        }).then((result) => {
+
+            if (result.isConfirmed) {
+
+                const remainingBook = book.filter(b => b.bookId !== id);
+
+                setBook(remainingBook);
+
+                removeDetails(id);
+
+                Swal.fire(
+                    "Removed!",
+                    "Book removed from wishlist.",
+                    "success"
+                );
+            }
+
+        });
 
     }
 
-    // get array DB;
-    useEffect(() => {
-        const getAllArrayDB = getStoredBook();
-        // console.log(getAllArrayDB);
-        const matchId = [];
-        for (const item of getAllArrayDB) {
-            // console.log(item);
-            const singleId = allBooks.find(book => book.bookId === item);
-            if (singleId) {
-                matchId.push(singleId)
-            }
-        }
-        setBook(matchId)
-    }, [])
 
+    useEffect(() => {
+
+        const storedBookIds = getStoredBook();
+
+        const myWishList = allBooks.filter(book =>
+            storedBookIds.includes(book.bookId)
+        );
+
+        setBook(myWishList);
+
+    }, [allBooks]);
 
 
     return (
         <div>
-            <h1 className='font-bold text-2xl text-center mt-5'>All WishList ({book.length})</h1>
+
+            <h1 className='font-bold text-2xl text-center mt-5'>
+                All WishList ({book.length})
+            </h1>
 
             <div >
+
                 {
-                    book.map(bk => <WishListBook handleRemove={handleRemove} key={bk.bookId} bk={bk}></WishListBook>)
+                    book.map(bk =>
+                        <WishListBook
+                            key={bk.bookId}
+                            bk={bk}
+                            handleRemove={handleRemove}
+                        />
+                    )
                 }
+
             </div>
+
         </div>
     );
 };
