e040b74bef31d88fbfb46fb46d2bd25a01c328a5
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-11 09:01:33 +0600

    fetch data another way

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+// import { useLoaderData } from 'react-router';
 import Books from '../Books/Books';
 const Home = () => {
-    const usersData = useLoaderData();
-    console.log(usersData)
-        ;
+    // const usersData = useLoaderData();
+    // console.log(usersData)
+    //     ;
     return (
 
         <div>
-            <h1>ima hobe</h1>
-            {
+            {/* <h1>All Users {usersData.length}</h1> */}
+            {/* {
                 usersData.map(user=><Books key={user.id} user={user}></Books>)
-            }
+            } */}
         </div>
     );
 };
